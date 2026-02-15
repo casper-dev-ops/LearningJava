2c1e8b578867fccd1c89f90dc1d35d19b86f19f9
Casper <casper_hktmr@tuta.io>
2026-02-15 00:06:08 -0500

    Add additional console logs for username and displayName

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 console.log("Hello JavaScript");
 console.log(34 * 809)
+
+
+ let username = "Casper";
+ console.log(username);
+ username = "Casper Andersen";
+ console.log(username); 
+ username = "Casper is a great guy";
+ console.log(username);
+
+ const displayName = "Casper Andersen";
+ console.log(displayName);  
