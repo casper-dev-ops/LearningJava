f1eb6beb83e38db7b020edf668523839478d7daf
Casper <casper_hktmr@tuta.io>
2026-02-15 01:23:20 -0500

    Add arithmetic functions and demonstrate usage with console logs

--- app.js
+++ app.js
@@ -1,64 +1,110 @@
-// console.log("Hello JavaScript");
-// console.log(34 * 809)
+// // console.log("Hello JavaScript");
+// // console.log(34 * 809)
 
 
-//  let username = "Casper";
-//  console.log(username);
-//  username = "Casper Andersen";
-//  console.log(username); 
-//  username = "Casper is a great guy";
-//  console.log(username);
+// //  let username = "Casper";
+// //  console.log(username);
+// //  username = "Casper Andersen";
+// //  console.log(username); 
+// //  username = "Casper is a great guy";
+// //  console.log(username);
 
-//  const displayName = "Casper Andersen";
-//  console.log(displayName);  
+// //  const displayName = "Casper Andersen";
+// //  console.log(displayName);  
 
-//  let islearning = true;
-//  console.log(islearning);
-//  islearning = false;
-//  console.log(islearning);
+// //  let islearning = true;
+// //  console.log(islearning);
+// //  islearning = false;
+// //  console.log(islearning);
 
-//  const bigMath = 9999999999999999;
-//  const smallMath = 0.0000000000000001;
+// //  const bigMath = 9999999999999999;
+// //  const smallMath = 0.0000000000000001;
 
-//  console.log(bigMath < smallMath);
-
-
-//  let age = 30;
-//  if (age > 18) {
-//      console.log("You are an adult");
-//  }
+// //  console.log(bigMath < smallMath);
 
 
-
-
-
-//  age = age + 1;
-//  console.log(age);
-
-
-//  console.log(5 === 5);
-//  console.log(5 === "5");
-//  console.log(5 == "5"); 
-
-const name = "Casper";
-let password = "12345678";
-if (password === "12345678") {
-    console.log("You are logged in as " + name);
-} else {
-    console.log("Wrong password");
-}
-
-
-let lanuage = "french";
-if (lanuage === "english") {
-    console.log("Hello");
-} else if (lanuage === "spanish") {
-    console.log("Hola");
-} else if (lanuage === "french") {
-    console.log("Bonjour");
-} else {
-    console.log("Language not supported");
-}
+// //  let age = 30;
+// //  if (age > 18) {
+// //      console.log("You are an adult");
+// //  }
 
 
 
+
+
+// //  age = age + 1;
+// //  console.log(age);
+
+
+// //  console.log(5 === 5);
+// //  console.log(5 === "5");
+// //  console.log(5 == "5"); 
+
+// const name = "Casper";
+// let password = "12345678";
+// if (password === "12345678") {
+//     console.log("You are logged in as " + name);
+// } else {
+//     console.log("Wrong password");
+// }
+
+
+// let lanuage = "french";
+// if (lanuage === "english") {
+//     console.log("Hello");
+// } else if (lanuage === "spanish") {
+//     console.log("Hola");
+// } else if (lanuage === "french") {
+//     console.log("Bonjour");
+// } else {
+//     console.log("Language not supported");
+// }
+
+// // let counter = 0;
+// // while (counter < 10){
+// //     console.log(counter);
+// //     counter ++;
+// // }
+
+
+// for(let i =0; i <10; i++){
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+// function sayHello (name) {
+//     console.log("Sup broski" + name);
+
+// }
+
+
+// sayHello(" Casper");
+
+// function addNumbers (num1, num2) {
+//      console.log(num1 + num2);
+// }
+
+// function subtractNumbers (num1, num2) {
+//      console.log(num1 - num2);
+// }
+
+// function multiplyNumbers (num1, num2) {
+//      console.log(num1 * num2);
+// }
+
+// function divideNumbers (num1, num2) {
+//      console.log(num1 / num2);
+// }
+
+// addNumbers(92, 8);
+// subtractNumbers(92, 8);
+// multiplyNumbers(92, 8);
+// divideNumbers(92, 8);
