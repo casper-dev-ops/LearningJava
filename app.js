9b9890ace852633892503fb91cb0ef7134679912
Casper <casper_hktmr@tuta.io>
2026-02-15 00:37:46 -0500

    Comment out console log statements for debugging purposes

--- app.js
+++ app.js
@@ -1,42 +1,64 @@
-console.log("Hello JavaScript");
-console.log(34 * 809)
+// console.log("Hello JavaScript");
+// console.log(34 * 809)
 
 
- let username = "Casper";
- console.log(username);
- username = "Casper Andersen";
- console.log(username); 
- username = "Casper is a great guy";
- console.log(username);
+//  let username = "Casper";
+//  console.log(username);
+//  username = "Casper Andersen";
+//  console.log(username); 
+//  username = "Casper is a great guy";
+//  console.log(username);
 
- const displayName = "Casper Andersen";
- console.log(displayName);  
+//  const displayName = "Casper Andersen";
+//  console.log(displayName);  
 
- let islearning = true;
- console.log(islearning);
- islearning = false;
- console.log(islearning);
+//  let islearning = true;
+//  console.log(islearning);
+//  islearning = false;
+//  console.log(islearning);
 
- const bigMath = 9999999999999999;
- const smallMath = 0.0000000000000001;
+//  const bigMath = 9999999999999999;
+//  const smallMath = 0.0000000000000001;
 
- console.log(bigMath < smallMath);
-
-
- let age = 30;
- if (age > 18) {
-     console.log("You are an adult");
- }
+//  console.log(bigMath < smallMath);
 
 
+//  let age = 30;
+//  if (age > 18) {
+//      console.log("You are an adult");
+//  }
 
 
 
- age = age + 1;
- console.log(age);
 
 
- console.log(5 === 5);
- console.log(5 === "5");
- console.log(5 == "5"); 
+//  age = age + 1;
+//  console.log(age);
+
+
+//  console.log(5 === 5);
+//  console.log(5 === "5");
+//  console.log(5 == "5"); 
+
+const name = "Casper";
+let password = "12345678";
+if (password === "12345678") {
+    console.log("You are logged in as " + name);
+} else {
+    console.log("Wrong password");
+}
+
+
+let lanuage = "french";
+if (lanuage === "english") {
+    console.log("Hello");
+} else if (lanuage === "spanish") {
+    console.log("Hola");
+} else if (lanuage === "french") {
+    console.log("Bonjour");
+} else {
+    console.log("Language not supported");
+}
+
+
 
